e5d1f356e2b52e36fdc963dc5c732b065fe44d21
koolksp <koolksp@gmail.com>
2014-11-22 13:34:30 +0530

    Initial commit

---
+++ src/main/java/com/sample/hello/Hello.java
@@ -0,0 +1,10 @@
+package com.sample.hello;
+
+
+public class Hello {
+
+    public static void main(String... args) {
+        System.out.println("Hello World!!");
+        System.out.println("Added some change");
+    }
+}
